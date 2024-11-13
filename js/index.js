22b93a23d238f17e7bbf1dd84150f983ecb51c3a
DinhHieu2004 <22130082@st.hcmuaf.edu.vn>
2024-11-13 14:55:12 +0700

    load header and footer

--- js/index.js
+++ js/index.js
@@ -1,14 +1,8 @@
 $(document).ready(function(){
     $("#footer-container").load("footer.html");
+    $("#header-container").load("header.html");
 
 
-    console.log("jQuery đã tải"); // Kiểm tra xem jQuery có hoạt động không
-    $("#header").load("header.html", function(response, status, xhr) {
-        if (status == "error") {
-            console.log("Lỗi khi load header:", xhr.status, xhr.statusText);
-        } else {
-            console.log("Header đã được tải thành công");
-        }
-    });
+    
 });
 
