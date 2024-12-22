0026dd33818e2162a047ff43c945de164f733b78
NhatHao-cryto <22130073@st.hcmuaf.edu.vn>
2024-12-22 16:39:54 +0700

    update form dang nhap

---
+++ src/main/java/com/example/web/controller/RegisterController.java
@@ -0,0 +1,9 @@
+package com.example.web.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name="RegisterController", value = "/register")
+public class RegisterController extends HttpServlet {
+
+}
