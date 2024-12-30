f4b25eb51d0876de93fdfa1d1270f500d4665d6a
NhatHao-cryto <22130073@st.hcmuaf.edu.vn>
2024-12-30 20:01:50 +0700

    Phan quyen cho admin va user

---
+++ src/main/java/com/example/web/controller/AdminPageController.java
@@ -0,0 +1,23 @@
+package com.example.web.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet(name = "adminPageController", value = "/admin")
+public class AdminPageController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session == null || !"admin".equals(session.getAttribute("role"))) {
+            response.sendRedirect("index.jsp"); // Chuyển hướng nếu không phải admin
+            return;
+        }
+        request.getRequestDispatcher("/admin/dashboard.html").forward(request, response); // Hiển thị dashboard
+    }
+}
